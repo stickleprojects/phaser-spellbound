9dd1598fb7e31fefb1e8f56ef8e6caff53e1e9d8
kieron <kieron.wray@gmail.com>
2020-06-13 13:09:40 +0100

    added knight graphics and inital level

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+/// <reference path="./typings/phaser.d.ts" />
+
+import { NinePatchPlugin } from '@koreez/phaser3-ninepatch';
 import Phaser from 'phaser';
 
 export default {
@@ -5,9 +8,30 @@ export default {
   parent: 'game',
   backgroundColor: '#33A5E7',
   scale: {
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 320,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
+  plugins: {
+    global: [
+      { key: 'NinePatchPlugin', plugin: NinePatchPlugin, start: true }
+    ]
+  },
+  render: {
+    pixelArt: true
+  },
+  // see photonstorm.github.io/phase3-docs/phaser.physics.arcade.html
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 750
+      },
+      debug: false,
+      debugShowVelocity: true,
+      debugShowBody: true,
+      debugShowStaticBody: true
+    }
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Phaser from 'phaser';
 import config from './config';
 import GameScene from './scenes/Game';
+//import Actions from './dialogs/actions';
 
 new Phaser.Game(Object.assign(config, {
   scene: [GameScene],
