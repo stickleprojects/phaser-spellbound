d927f52e263db51cd49da6137b40900acbc36720
kieron wray <kieron.wray@gmail.com>
2024-09-03 18:27:49 +0100

    added rounding for object/character posistions when placing onscreen

--- v2/spellbound-phaser/src/maps/characters.ts
+++ v2/spellbound-phaser/src/maps/characters.ts
@@ -3,6 +3,7 @@ export default function createCharacterMap() {
     const characterMap = new Map<String, {x:integer,y: integer}>();
     characterMap.set("left1", {x:0,y:0});
     characterMap.set("left2", {x:1,y:0});
+    
     characterMap.set("ladyrosmar_right", {x:4,y:0});
     characterMap.set("ladyrosmar_left", {x:5,y:0});
     characterMap.set("thor_right", {x:6,y:0});
@@ -15,7 +16,10 @@ export default function createCharacterMap() {
     characterMap.set("samsun_left", {x:13,y:0});
     characterMap.set("elrand_right", {x:14,y:0});
     characterMap.set("elrand_left", {x:15,y:0});
-
+    characterMap.set("knight_right", {x:0,y:1});
+    characterMap.set("orik_right", {x:6,y:1});
+    characterMap.set("wizard_right", {x:4,y:1});
+    
     return characterMap;
     
 }
--- v2/spellbound-phaser/src/scenes/GamePlay.ts
+++ v2/spellbound-phaser/src/scenes/GamePlay.ts
@@ -17,12 +17,12 @@ export class GamePlay extends Phaser.Scene {
     private map: Phaser.Tilemaps.Tilemap;
 
     private background: Phaser.GameObjects.Image;
-    private msg_text : Phaser.GameObjects.Text;
-    private map_alltiles: Phaser.Tilemaps.Tileset ;
-    private map_foregroundtiles: Phaser.Tilemaps.Tileset ;
-    private backgroundLayer: Phaser.Tilemaps.TilemapLayer ;
-    private foregroundLayer: Phaser.Tilemaps.TilemapLayer ;
-    private solidLayer: Phaser.Tilemaps.TilemapLayer ;
+    private msg_text: Phaser.GameObjects.Text;
+    private map_alltiles: Phaser.Tilemaps.Tileset;
+    private map_foregroundtiles: Phaser.Tilemaps.Tileset;
+    private backgroundLayer: Phaser.Tilemaps.TilemapLayer;
+    private foregroundLayer: Phaser.Tilemaps.TilemapLayer;
+    private solidLayer: Phaser.Tilemaps.TilemapLayer;
     private characterSprites: Phaser.GameObjects.GameObject[];
     private objectSprites: Phaser.GameObjects.GameObject[];
     characterLayer: Phaser.Types.Tilemaps.TiledObject[];
@@ -32,7 +32,7 @@ export class GamePlay extends Phaser.Scene {
     roomY: number;
     roomWidth: number;
     roomHeight: number;
-     characterMap: Map<String, { x: integer; y: integer; }>;
+    characterMap: Map<String, { x: integer; y: integer; }>;
     objectMap: Map<String, { x: integer; y: integer; }>;
     cameraController: any;
     constructor() {
@@ -44,10 +44,10 @@ export class GamePlay extends Phaser.Scene {
     }
     create() {
         this.camera = this.cameras.main
-        this.cursors=this.input.keyboard?.createCursorKeys()
-        
+        this.cursors = this.input.keyboard?.createCursorKeys()
+
         // init the systems
-        
+
         this.movementSystem = createMovementSystem()
         this.spriteSystem = createSpriteSystem(this, [""])
 
@@ -55,23 +55,23 @@ export class GamePlay extends Phaser.Scene {
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x000000);
         this.camera.setZoom(0.5);
+
+        /*
+                this.background = this.add.image(512, 384, 'background');
+                this.background.setAlpha(0.5);
         
-/*
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+                this.msg_text = this.add.text(512, 384, 'Gameplay scene', {
+                    fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
+                    stroke: '#000000', strokeThickness: 8,
+                    align: 'center'
+                });
+                this.msg_text.setOrigin(0.5);
+        */
 
-        this.msg_text = this.add.text(512, 384, 'Gameplay scene', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
-*/
+        this.map = this.make.tilemap({ key: 'levels', tileWidth: 16, tileHeight: 16 });
 
-        this.map = this.make.tilemap({key: 'levels', tileWidth: 16, tileHeight: 16});
-
-        this.map_alltiles = this.map.addTilesetImage('alltiles','map_background')!
-        this.map_foregroundtiles = this.map.addTilesetImage('foregroundtiles','map_foreground')!
+        this.map_alltiles = this.map.addTilesetImage('alltiles', 'map_background')!
+        this.map_foregroundtiles = this.map.addTilesetImage('foregroundtiles', 'map_foreground')!
 
         this.backgroundLayer = this.map.createLayer('background', this.map_alltiles)!
         this.foregroundLayer = this.map.createLayer('foreground', this.map_foregroundtiles)!
@@ -80,43 +80,63 @@ export class GamePlay extends Phaser.Scene {
         this.doorLayer = this.map.getObjectLayer('doorobjects')?.objects!
         this.objectLayer = this.map.getObjectLayer('objects')?.objects!
 
-        
-        this.characterMap =createCharacterMap();
+
+        this.characterMap = createCharacterMap();
 
         this.objectMap = createObjectMap();
 
-        
+
         this.roomX = -9;
         this.roomY = -8;
-        this.roomWidth=8*4;
-        this.roomHeight=8;
-       
+        this.roomWidth = 8 * 4;
+        this.roomHeight = 8;
+
+        const objectTileHeight = 16;
+        const objectTileWidth = 16;
+        const objectTilemapWidth = 16;
+
+        const objecthalfW = objectTileWidth / 2;
+        const objecthalfH = objectTileHeight / 2;
+
         // create the objets
         this.objectLayer.forEach(o => {
-            const objectName=o.name;
-            const objectPosition = this.objectMap.get(objectName);
+            const objectName = o.name;
+            const objectTilePosition = this.objectMap.get(objectName);
 
-            const mapWidth=16;
-            if(!objectPosition) {
-                console.log("Failed to find object "+objectName)
+            const mapWidth = objectTilemapWidth;
+            if (!objectTilePosition) {
+                console.log("Failed to find object " + objectName)
             } else {
 
-            const index=(objectPosition.y * mapWidth) + objectPosition.x;
+                const index = (objectTilePosition.y * mapWidth) + objectTilePosition.x;
+                const pixelX = Math.ceil(o.x! / 16) * 16;
+                const pixelY = Math.ceil(o.y! / 16) * 16;
 
-            this.add.sprite(o.x! + 4, o.y! + 8, 'objects',index);
+                this.add.sprite(pixelX, pixelY + objecthalfH, 'objects', index);
             }
         })
 
-        this.characterLayer.forEach(o => {
-            const characterName=o.name+"_right";
-            const characterPosition = this.characterMap.get(characterName);
-            const mapWidth=16;
-            if(!characterPosition ) {
-                console.log("Cannot find character texture for "+characterName);
-            } else {
-                const index=(characterPosition.y * mapWidth) + characterPosition.x;
+        const characterTileHeight = 32;
+        const charaterTileWidth = 16;
+        const characterTilemapWidth = 16;
 
-                this.add.sprite(o.x! + 4, o.y! + 8, 'characters', index);
+        const characterhalfH = characterTileHeight / 2;
+        const characterhalfW = charaterTileWidth / 2;
+
+        this.characterLayer.forEach(o => {
+            const characterName = o.name + "_right";
+            const characterTileCoords = this.characterMap.get(characterName);
+            const mapWidth = characterTilemapWidth;
+
+            if (!characterTileCoords) {
+                console.log("Cannot find character texture for " + characterName);
+            } else {
+                const index = (characterTileCoords.y * mapWidth) + characterTileCoords.x;
+
+                const pixelX = Math.ceil(o.x! / 16) * 16;
+                const pixelY = Math.ceil(o.y! / 16) * 16;
+
+                this.add.sprite(pixelX + characterhalfW, pixelY + characterhalfH, 'characters', index);
             }
         })
 
@@ -132,14 +152,14 @@ export class GamePlay extends Phaser.Scene {
             zoomOut: this.input!.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.E),
         };
         this.cameraController = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-       
+
     }
     init(data) {
         // everything is loaded, so display stuff
         // data is the params passed to this scene with this.scene.start(key,data)
 
         // in this example, i will share the world with one passed in
-        if(data.world) {
+        if (data.world) {
             this.world = data.world
         } else {
             this.world = createWorld()
@@ -152,7 +172,7 @@ export class GamePlay extends Phaser.Scene {
         this.cameraController.update(delta);
 
         // tick the input system and other systems maybe
-        
+
     }
     drawRoom() {
 
