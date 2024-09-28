f7593ffe5768bc653f8c07533ecc6f1b8cc4bfd2
kieron wray <kieron.wray@gmail.com>
2024-09-28 17:27:50 +0100

    simplifed folder structure and remvoed v1

--- v1/downloads/freeknight/dragonbones/anims/small/combine_knight_images.sh
+++
@@ -1,6 +0,0 @@
-montage small/Knight_Walk*.png -background transparent -gravity south -trim -geometry 32x32 hero-walk.png
-montage small/Knight_Idle*.png -background transparent -gravity south -trim -geometry 32x32 hero-idle.png
-montage small/Knight_Jump*.png -background transparent -gravity south -trim -geometry 32x32 hero-jump.png
-montage small/Knight_Pivot*.png -background transparent -gravity south -trim -geometry 32x32 hero-pivot.png
-montage small/Knight_Dead*.png -background transparent -gravity south -trim -geometry 32x32 hero-dead.png
-
