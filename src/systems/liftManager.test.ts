5bbb452e391858268ddc0a0fe4863b505d36939f
Kieron Wray <kieron.wray@gmail.com>
2025-04-13 15:02:39 +0100

    updated whitespace

--- src/systems/liftManager.test.ts
+++ src/systems/liftManager.test.ts
@@ -1,87 +1,80 @@
-import { IDoor, LiftManager } from './liftManager';
-import { any, mock } from 'jest-mock-extended';
+import { IDoor, LiftManager } from "./liftManager";
+import { any, mock } from "jest-mock-extended";
 
 describe("door test", () => {
-    describe('close', () => {
-        // it('should play the close animation', async () => {
-        //     const spr = mock<IDoorSprite>();
-        //     spr.PlayAnimation.mockImplementationOnce((animationName: string) => {
-        //         console.log(animationName);
-        //         return Promise.resolve();
-        //     })
-
-        //     const d = new Door(spr);
-
-        //     await d.CloseAsync();
-
-        //     expect(spr.PlayAnimation).toHaveBeenCalledWith('close');
-
-        // })
-        // it('should set closed state after the close animation finishes', async () => {
-        //     const spr = mock<IDoorSprite>();
-        //     spr.PlayAnimation.mockImplementationOnce((animationName: string) => {
-        //         console.log(animationName);
-
-        //         //  spr.OnAnimationCompleted(animationName);
-        //         return Promise.resolve();
-        //     });
-
-        //     const d = new Door(spr);
-
-        //     await d.CloseAsync();
-
-        //     expect(d.isClosed).toBe(true);
-
-        // })
-    })
-})
+  describe("close", () => {
+    // it('should play the close animation', async () => {
+    //     const spr = mock<IDoorSprite>();
+    //     spr.PlayAnimation.mockImplementationOnce((animationName: string) => {
+    //         console.log(animationName);
+    //         return Promise.resolve();
+    //     })
+    //     const d = new Door(spr);
+    //     await d.CloseAsync();
+    //     expect(spr.PlayAnimation).toHaveBeenCalledWith('close');
+    // })
+    // it('should set closed state after the close animation finishes', async () => {
+    //     const spr = mock<IDoorSprite>();
+    //     spr.PlayAnimation.mockImplementationOnce((animationName: string) => {
+    //         console.log(animationName);
+    //         //  spr.OnAnimationCompleted(animationName);
+    //         return Promise.resolve();
+    //     });
+    //     const d = new Door(spr);
+    //     await d.CloseAsync();
+    //     expect(d.isClosed).toBe(true);
+    // })
+  });
+});
 describe("lift manager test", () => {
+  it("should close all lift doors", async () => {
+    let doors: IDoor[] = [];
 
-    it('should close all lift doors', async () => {
+    const d = mock<IDoor>({ Name: "door1" });
+    doors.push(d);
 
-        let doors: IDoor[] = [];
+    const im = mock<Phaser.Input.InputPlugin>();
+    const kb = mock<Phaser.Input.Keyboard.KeyboardPlugin>();
+    kb.addKey
+      .calledWith(any())
+      .mockReturnValue(mock<Phaser.Input.Keyboard.Key>());
+    im.keyboard = kb;
+    const s = mock<Phaser.Sound.WebAudioSoundManager>();
+    await LiftManager.CreateAsync(doors, im, s);
 
-        const d = mock<IDoor>({ Name: 'door1' });
-        doors.push(d);
+    expect(d.CloseAsync).toHaveBeenCalled();
+  });
 
+  describe("when opening one door", () => {
+    it("should close the other doors", async () => {
+      let doors: IDoor[] = [];
 
-        const im = mock<Phaser.Input.InputPlugin>();
-        const kb = mock<Phaser.Input.Keyboard.KeyboardPlugin>();
-        kb.addKey.calledWith(any()).mockReturnValue(mock<Phaser.Input.Keyboard.Key>());
-        im.keyboard = kb;
-        await LiftManager.CreateAsync(doors, im);
+      const md1 = mock<IDoor>({ Name: "door1" });
+      mock(md1).CloseAsync.mockReturnValue(Promise.resolve(true));
 
-        expect(d.CloseAsync).toHaveBeenCalled();
+      const md2 = mock<IDoor>({ Name: "door2" });
+      mock(md2).CloseAsync.mockReturnValue(Promise.resolve(true));
 
-    })
+      const md3 = mock<IDoor>({ Name: "door3" });
+      mock(md3).CloseAsync.mockReturnValue(Promise.resolve(true));
 
-    describe('when opening one door', () => {
-        it('should close the other doors', async () => {
-            let doors: IDoor[] = [];
+      doors.push(md1);
+      doors.push(md2);
+      doors.push(md3);
 
-            const md1 = mock<IDoor>({ Name: 'door1' });
-            mock(md1).CloseAsync.mockReturnValue(Promise.resolve(true));
+      const im = mock<Phaser.Input.InputPlugin>();
+      const kb = mock<Phaser.Input.Keyboard.KeyboardPlugin>();
+      kb.addKey
+        .calledWith(any())
+        .mockReturnValue(mock<Phaser.Input.Keyboard.Key>());
+      im.keyboard = kb;
+      const s = mock<Phaser.Sound.WebAudioSoundManager>();
+      const d = mock<IDoor>();
+      const sut = await LiftManager.CreateAsync(doors, im, s);
+      await sut.callLiftAsync(d, true);
 
-            const md2 = mock<IDoor>({ Name: 'door2' });
-            mock(md2).CloseAsync.mockReturnValue(Promise.resolve(true));
-
-            const md3 = mock<IDoor>({ Name: 'door3' });
-            mock(md3).CloseAsync.mockReturnValue(Promise.resolve(true));
-
-            doors.push(md1);
-            doors.push(md2);
-            doors.push(md3);
-
-            const im = mock<Phaser.Input.InputPlugin>();
-            const kb = mock<Phaser.Input.Keyboard.KeyboardPlugin>();
-            kb.addKey.calledWith(any()).mockReturnValue(mock<Phaser.Input.Keyboard.Key>());
-            im.keyboard = kb;
-            const sut = await LiftManager.CreateAsync(doors, im);
-            await sut.callLiftAsync(2);
-
-            doors.forEach(d => expect(d.CloseAsync).toHaveBeenCalled());
-            expect(doors[2].OpenAsync).toHaveBeenCalled();
-
-        })
-    })
-})
+      doors.forEach((d) => expect(d.CloseAsync).toHaveBeenCalled());
+      expect(doors[2].OpenAsync).toHaveBeenCalled();
+    });
+  });
+});
